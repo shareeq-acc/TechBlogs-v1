4abbc47641cc0c56a1be6ee51f4c1761d7f44017
shareeq-acc <109340294+shareeq-acc@users.noreply.github.com>
2022-07-15 15:03:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ dbConnection();
 // --------------------------deployment------------------------------
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static(path.join(__dirname, "/client/build")));
-  app.use(express.static("/client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
+//   app.use(express.static("/client/build"));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
